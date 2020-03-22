ed3a07171644fc8466de5432b5561e94d6e9c994
hzengin <hzengin@genbil.com>
2020-03-23 00:18:55 +0300

    webapi projesi eklendi

---
+++ src/server/WebAPI/Controllers/BaseApiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        [NonAction]
+        public IActionResult Success(object data = default(object))
+        {
+            return Ok(data);
+        }
+
+        [NonAction]
+        public IActionResult Error(string errorMessage = default(string), int? errorCode = (int?)null, object data = default(object))
+        {
+            return BadRequest(new
+            {
+                ErrorMessage = errorMessage,
+                ErrorCode = errorCode,
+            });
+        }
+    }
+}
