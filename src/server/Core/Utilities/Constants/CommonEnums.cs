3eae9b6bf6974b9c03ee179f43f780e95e9e436c
hzengin <hzengin@genbil.com>
2020-03-23 00:20:11 +0300

    genel tanımlar ve enum'lar için class eklendi

---
+++ src/server/Core/Utilities/Constants/CommonEnums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Constants
+{
+    public enum EncryptType
+    {
+        AES = 1,
+        Rijindael = 2,
+        RSA = 3,
+        TripleDes = 4
+    }
+}
