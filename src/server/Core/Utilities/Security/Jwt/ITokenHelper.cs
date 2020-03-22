4b175f4fb33bcbd19ef79123c6183a55c910a9c5
hzengin <hzengin@genbil.com>
2020-03-23 00:19:42 +0300

    güvenlik işlemleri için helper class'lar eklendi

---
+++ src/server/Core/Utilities/Security/Jwt/ITokenHelper.cs
@@ -0,0 +1,10 @@
+﻿using Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Core.Utilities.Security.Jwt
+{
+    public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user, List<string> roles);
+    }
+}
---
+++ src/server/Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -0,0 +1,65 @@
+﻿using Core.Utilities.Security.Encyption;
+using Entities.Concrete;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Core.Utilities.Security.Jwt
+{
+    public class JwtHelper : ITokenHelper
+    {
+        public IConfiguration Configuration { get; }
+        private TokenOptions _tokenOptions;
+        private DateTime _accessTokenExpiration;
+        public JwtHelper(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _tokenOptions = Configuration.GetSection("TokenOptions") as TokenOptions;
+        }
+        public AccessToken CreateToken(User user, List<string> roles)
+        {
+            _accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccessTokenExpiration);
+            var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+            var signingCredentials = SigningCredentialsHelper.CreateSigningCredentials(securityKey);
+            var jwt = CreateJwtSecurityToken(_tokenOptions, user, signingCredentials, roles);
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var token = jwtSecurityTokenHandler.WriteToken(jwt);
+
+            return new AccessToken
+            {
+                Token = token,
+                Expiration = _accessTokenExpiration
+            };
+
+        }
+
+        public JwtSecurityToken CreateJwtSecurityToken(TokenOptions tokenOptions, User user,
+            SigningCredentials signingCredentials, List<string> roles)
+        {
+            var jwt = new JwtSecurityToken(
+                issuer: tokenOptions.Issuer,
+                audience: tokenOptions.Audience,
+                expires: _accessTokenExpiration,
+                notBefore: DateTime.Now,
+                claims: SetClaims(user, roles),
+                signingCredentials: signingCredentials
+            );
+            return jwt;
+        }
+
+        private IEnumerable<Claim> SetClaims(User user, List<string> roles)
+        {
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+            claims.Add(new Claim(ClaimTypes.Name, $"{user.FirstName} {user.LastName}"));
+            roles.ToList().ForEach(role => claims.Add(new Claim(ClaimTypes.Role, role)));
+
+            return claims;
+        }
+    }
+}
